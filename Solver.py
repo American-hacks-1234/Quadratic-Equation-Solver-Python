8635970d9bf26df846d100259b22efa947828dda
American Awesome Sauce Software <russelltm1@s.dcsdk12.org>
2026-08-27 10:22:02 -0600

    Update Solver.py
    Improved space usage 
    Improved visuals
    (log removed)

--- Solver.py
+++ Solver.py
@@ -1,42 +1,26 @@
-import time
 import math
-def now_ts():
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-LOGNAME="log"
-def append_my_log(msg):
-    try:
-        with open(LOGNAME, "a", encoding="utf-8") as fh:
-            fh.write(f"{now_ts()} - {msg}\n")
-    except Exception:
-        pass
-msg="this is the log"
-now_ts()
-import math
-def text(z):
-  print(z)
-a=input("A: ")
-b=input("B: ")
-c=input("C: ")
+
 def quadraticsolver(a, b, c):
   a=int(a)
   b=int(b)
   c=int(c)
-  text(f"ORIGINAL: x=(-{b}±sqrt({b}^2-4*{a}*{c}))/(2*{a})")
+  print(f"ORIGINAL: X = (-{b}±√({b}^2-4•{a}•{c}))÷(2•{a})")
   B=b*-1
   b=b*b
   f=2*a
   y=(b-4*a*c)
-  text(f"STEP 1: x=({B}±sqrt({y}))/{f}")
+  print("")
+  print(f"STEP 1: X = ({B}±√({y}))÷{f}")
   result=math.sqrt(y)
-  text(f"STEP 2: ({B}±{result})/{f}")
+  print("")
+  print(f"STEP 2: X = ({B}±{result})÷{f}")
   Z=B+result
   z=B+-result
-  text(f"STEP 3: {Z}/{f}, {z}/{f}")
+  print("")
+  print(f"STEP 3: X = {Z}÷{f}, {z}÷{f}")
   x0=Z/f
   x1=z/f
-  text(f"ans: {x0}, {x1}")
-  str(x0)
-  str(x1)
-  q=f"x=[{x0}, {x1}]"
-  append_my_log(q)
-quadraticsolver(a, b ,c)
+  print("")
+  print(f"ANS: X = {x0}, {x1}")
+
+quadraticsolver(input("a: "), input("b: ") ,input("c: "))
